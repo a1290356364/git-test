98a2f0fffc5b4b58023dc607652a8c287a846fd3
zhangsan <zhangsan@itcast.cn>
2019-03-14 16:45:59 +0800

    2019-3-14第一次修改Frist_git_project项目

--- Frist_git_project/src/com/itheima/git/frist.java
+++ Frist_git_project/src/com/itheima/git/frist.java
@@ -13,4 +13,9 @@ public class frist {
         System.out.println("hello git project");
         System.out.println("hello git project");
     }
+    public void test(){
+        int i=0;
+        i+=10;
+        System.out.println(i);
+    }
 }
