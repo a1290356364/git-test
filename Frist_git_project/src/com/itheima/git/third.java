d62da3e1b7b5c3d1c5e3ded3db9183f2b5ea8278
zhangsan <zhangsan@itcast.cn>
2019-03-14 18:21:37 +0800

    主干修改代码

--- Frist_git_project/src/com/itheima/git/third.java
+++ Frist_git_project/src/com/itheima/git/third.java
@@ -2,6 +2,9 @@ package com.itheima.git;
 
 public class third {
     public void test(){
+
+        System.out.println("hello third");
+        System.out.println("hello third");
         System.out.println("hello third");
     }
 }
